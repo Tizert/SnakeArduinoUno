a0bafcabb99cca96c8e340282df10e98138e4f8e
DESKTOP-A152RG0\KetsaT <smirnovplsg@gmail.com>
2024-09-04 23:07:46 +0700

    added joystick implementation and beautify code

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,7 @@
 #include <Arduino.h>
+#include <cppQueue.h>
 
-void my_printf(const char *format, ...) {
-    const uint8_t MAX_STRING_SIZE = 64;
-    char buf[MAX_STRING_SIZE];
-
-    va_list args;
-    va_start(args, format);
-    vsnprintf(buf, MAX_STRING_SIZE, format, args);
-    va_end(args);
-    Serial.print(buf);
-}
+#define	IMPLEMENTATION	FIFO
 
 #define LED_C1 2
 #define LED_C2 4
@@ -27,23 +19,101 @@ void my_printf(const char *format, ...) {
 #define VRX A0
 #define VRY A1
 
+cppQueue	q_snake(25, 25, IMPLEMENTATION);
+
 uint8_t d_columns[] = {LED_C1, LED_C2, LED_C3, LED_C4, LED_C5};
 uint8_t d_rows[]    = {LED_R1, LED_R2, LED_R3, LED_R4, LED_R5};
 
-void led_matrix(uint8_t r, uint8_t c) {
-    static uint8_t prev_row = 0;
-    static uint8_t prev_col = 0;
-    pinMode(d_rows[prev_row], INPUT);
-    pinMode(d_columns[prev_col], INPUT);
+const uint_fast8_t MIDDLE_XY = 128;
+const uint_fast8_t OFFSET_XY = 48;
+const uint8_t ROWS = 5;
+const uint8_t COLUMNS = 5;
+enum Direction {None, Up, Down, Left, Right};
 
+uint32_t start_a = 0;
+uint32_t start_b = 0;
+uint8_t last_direction = 0;
+
+uint8_t currRow = 0;
+uint8_t currCol = 0;
+
+void my_printf(const char *format, ...) {
+    const uint8_t MAX_STRING_SIZE = 64;
+    char buf[MAX_STRING_SIZE];
+
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buf, MAX_STRING_SIZE, format, args);
+    va_end(args);
+    Serial.print(buf);
+    // my_printf("X axis is =%d Y axis is %d\r\n", xAxis, yAxis);
+}
+
+Direction determineDirection(int x, int y) {
+    Direction xDir = Direction::None;
+    Direction yDir = Direction::None;
+    if (x > MIDDLE_XY + OFFSET_XY) {
+        xDir = Direction::Up;
+    }
+    if (x < MIDDLE_XY - OFFSET_XY) {
+        xDir = Direction::Down;
+    }
+    if (y > MIDDLE_XY + OFFSET_XY) {
+        yDir = Direction::Right;
+    }
+    if (y < MIDDLE_XY - OFFSET_XY) {
+        yDir = Direction::Left;
+    }
+
+    if (yDir == Direction::None) {
+        return xDir;
+    } else if (xDir == Direction::None) {
+        return yDir;
+    }
+
+    return Direction::None;
+}
+
+void led_matrix(uint8_t r, uint8_t c) {
     pinMode(d_rows[r], OUTPUT);
     pinMode(d_columns[c], OUTPUT);
     digitalWrite(d_columns[c], HIGH);
     digitalWrite(d_rows[r], LOW);
-    prev_row = r;
-    prev_col = c;
     pinMode(d_rows[r], INPUT);
     pinMode(d_columns[c], INPUT);
+}
+
+void sosok_direction() {
+    uint8_t xAxis = analogRead(VRX); 
+    uint8_t yAxis = analogRead(VRY);
+    xAxis = map(xAxis, 0, 169, 0, 255);
+    yAxis = map(yAxis, 0, 169, 0, 255);
+
+    uint8_t nowDirection = determineDirection(xAxis, yAxis); // Direction get
+
+    if (last_direction != nowDirection){
+        my_printf("X axis is =%d Y axis is %d\r\n", xAxis, yAxis);
+        switch (nowDirection)
+        {
+        case Direction::Up :
+            currRow = (currRow + 1) % ROWS;
+            break;
+        case Direction::Down :
+            currRow = (currRow - 1) < 0 ? ROWS - 1 : currRow - 1;
+            break;
+        case Direction::Right :
+            currCol = (currCol + 1) % COLUMNS;
+            break;
+        case Direction::Left :
+            currCol = (currCol - 1) < 0 ? COLUMNS - 1 : currCol - 1;
+            break;
+        case Direction::None :
+            break;
+        }
+        Serial.print(nowDirection);
+        Serial.print("\n");
+        last_direction = nowDirection;
+    }   
 }
 
 void setup() {
@@ -68,33 +138,15 @@ void setup() {
     Serial.println("Start code block");
 }
 
-uint32_t start_b = 0;
-uint32_t start_d = 0;
-
 void loop() {
-    if (millis() - start_b >= 0) {
-        uint8_t xAxis = analogRead(VRX); 
-        uint8_t yAxis = analogRead(VRY);
-        xAxis = map(xAxis, 0, 169, 0, 255);
-        yAxis = map(yAxis, 0, 169, 0, 255);
-        my_printf("X axis is =%d Y axis is %d\r\n", xAxis, yAxis);
-        delay(100);
+    if (millis() - start_a >= 99) {
+        sosok_direction();
+        start_a = millis();
+    }
+
+    led_matrix(currRow, currCol);
+
+    if (millis() - start_b >= 1000) {
         start_b = millis();
     }
-    // if (millis() - start_b >= 0) {
-    //     for (uint8_t row = 0; row < 4; row++) {
-    //         pinMode(rows[row], INPUT_PULLUP);
-    //         for (uint8_t col = 0; col < 4; col++) {
-    //             pinMode(columns[col], OUTPUT);
-    //             digitalWrite(columns[col], LOW);
-    //             if (digitalRead(rows[row]) == 0) {
-    //                 // my_printf("Ass = S%d\r\n", row * 4 + col + 1);
-    //                 led_matrix(row, col);
-    //             }
-    //             pinMode(columns[col], INPUT);
-    //         }
-    //         pinMode(rows[row], INPUT);
-    //     }
-    //     start_b = millis();
-    // }
 }
